e8b4315939ec0e32c39aa16d832475c1972efc8c
StefThom <jaapvdbelt@gmail.com>
2021-03-16 17:37:22 +0100

    Update to spring-boot 2.4.3 and camunda-bpm 7.14.0

--- basic/gateway-exclusive/run_process_scenario_03.sh
+++ basic/gateway-exclusive/run_process_scenario_03.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'http://localhost:8080/rest/process-definition/key/order-coffee/start' --header 'Content-Type: application/json' --data-raw '{
+curl --location --request POST 'http://localhost:8080/engine-rest/process-definition/key/order-coffee/start' --header 'Content-Type: application/json' --data-raw '{
      "variables": {
          "order": {
              "value": "Nothing",
