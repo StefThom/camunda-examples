e2156a7117db5c915a0a3010f581f8d50d397db2
StefThom <jaapvdbelt@gmail.com>
2021-03-16 16:35:51 +0100

    Changed to latest release of spring-boot (2.4.3) and latest release of camunda-bpm (7.14.0)

--- basic/gateway-event-based/run_process_scenario_01_Fail.sh
+++ basic/gateway-event-based/run_process_scenario_01_Fail.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'http://localhost:8080/rest/message' --header 'Content-Type: application/json' --data-raw '{
+curl --location --request POST 'http://localhost:8080/engine-rest/message' --header 'Content-Type: application/json' --data-raw '{
      "messageName": "payment-failed-message",
      "businessKey": "business-key-123"
 }'
